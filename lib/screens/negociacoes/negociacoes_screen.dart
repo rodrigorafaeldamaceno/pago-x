3b436f1a82f2eac53671e003ca445c605b61d0d1
Arthur <arthur.04.pb@gmail.com>
2020-05-03 20:28:02 -0300

    estilizando a NegociacaoScreen.dart

--- lib/screens/negociacoes/negociacoes_screen.dart
+++ lib/screens/negociacoes/negociacoes_screen.dart
@@ -2,36 +2,127 @@ import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 
 class NegociacaoScreen extends StatelessWidget {
-  GlobalKey _scaffoldKey = GlobalKey<ScaffoldState>();
+  final GlobalKey _scaffoldKey = GlobalKey<ScaffoldState>();
 
   Widget cardNegociacao({String item}) {
     return ExpansionTile(
-      title: Text(item),
+      title: Text(
+        item,
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
       children: <Widget>[
-        Column(
-          children: <Widget>[
-            Text(item),
-            Row(
-              children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    Text(item),
-                    Text(item),
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    Text(item),
-                    Text(item),
-                  ],
-                ),
-              ],
-            ),
-            RaisedButton(
-              onPressed: () {},
-              child: Text(item),
-            )
-          ],
+        Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Text('ID #5424',
+                      style: TextStyle(
+                        color: Colors.black45,
+                      )),
+                ],
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Text('Comprador',
+                          style: TextStyle(
+                            fontSize: 14,
+                          )),
+                      Text('Ricardo Smith',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          )),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Text('Preço',
+                          style: TextStyle(
+                            fontSize: 16,
+                          )),
+                      Text('R\$ 2.000,00',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          )),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Text('Vendedor',
+                          style: TextStyle(
+                            fontSize: 14,
+                          )),
+                      Text('José da Silva',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          )),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[                     
+                      Row(
+                        children: <Widget>[
+                           Icon(Icons.star),
+                          Text('4.6/5.0',
+
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.black45,
+                              )),
+                        ],
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Icon(Icons.location_on),
+                          Text('3 km de distância',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.black45,
+                              )),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  RaisedButton(
+                    elevation: 5,
+                    onPressed: () {},
+                    color: Colors.green,
+                    child: Text(
+                      'VER DETALHES',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         )
       ],
     );
@@ -46,22 +137,27 @@ class NegociacaoScreen extends StatelessWidget {
       body: Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
+          SizedBox(
+            height: 24,
+          ),
           Container(
             padding: EdgeInsets.symmetric(horizontal: 20),
             width: double.infinity,
             height: 45,
-            color: Colors.green,
+            color: Colors.white,
             child: Text(
               'Negociações',
-              textAlign: TextAlign.end,
+              textAlign: TextAlign.start,
               style: TextStyle(
                 fontSize: 24,
-                color: Colors.white,
+                fontWeight: FontWeight.w500,
+                color: Theme.of(context).accentColor,
               ),
             ),
           ),
           Container(
             height: size.height * 0.7,
+            padding: EdgeInsets.symmetric(horizontal: 20),
             child: DefaultTabController(
               length: 2,
               child: Scaffold(
@@ -80,8 +176,8 @@ class NegociacaoScreen extends StatelessWidget {
                       ),
                       indicatorWeight: 3.0,
                       tabs: [
-                        Tab(text: "Em andamento"),
-                        Tab(text: "Finalizadas"),
+                        Tab(text: "EM ANDAMENTO"),
+                        Tab(text: "FINALIZADAS"),
                       ],
                     ),
                   ),
@@ -92,13 +188,19 @@ class NegociacaoScreen extends StatelessWidget {
                       ListView.builder(
                         itemCount: 3,
                         itemBuilder: (BuildContext context, int index) {
-                          return cardNegociacao(item: 'Macbook');
+                          return Card(
+                            elevation: 5,
+                            child: cardNegociacao(item: 'Macbook'),
+                          );
                         },
                       ),
                       ListView.builder(
                         itemCount: 3,
                         itemBuilder: (BuildContext context, int index) {
-                          return cardNegociacao(item: 'Playstation');
+                          return Card(
+                            elevation: 5,
+                            child: cardNegociacao(item: 'Playstation'),
+                          );
                         },
                       ),
                     ],
