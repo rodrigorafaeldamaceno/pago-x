09598baf325907897160f980fddf47a2d2862a78
Arthur <arthur.04.pb@gmail.com>
2020-05-02 21:02:47 -0300

    tela inicial procurar produto

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -26,9 +26,53 @@ class HomeScreen extends StatelessWidget {
         physics: NeverScrollableScrollPhysics(),
         children: <Widget>[
           Center(
-            child: Text(
-              'Pagina 1',
-              style: TextStyle(fontSize: 50),
+            child: Container(
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'O que você está procurando?',
+                    style: TextStyle(
+                        fontSize: 24, color: Theme.of(context).accentColor),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Material(
+                    elevation: 2,
+                    child: TextField(                      
+                      decoration: InputDecoration(
+                        fillColor: Colors.white,
+                        filled: true,
+                        contentPadding: EdgeInsets.all(16),
+                        suffixIcon: Icon(Icons.mic),
+                        prefixIcon: Icon(Icons.search),
+                        labelText: 'Digite aqui...',
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  RaisedButton(
+                    onPressed: () {},
+                    elevation: 6,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 140,
+                      vertical: 16,
+                    ),
+                    child: Text(
+                      'PRÓXIMO',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                    color: Theme.of(context).accentColor,
+                  ),
+                ],
+              ),
             ),
           ),
           Center(
